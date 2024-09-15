96b977a84639de4ed62befc9ca8b7d87fc0016b6
JOAQUIN BEJAR <jb@taunais.com>
2024-09-15 19:39:21 +0100

    Document CurveOperations trait methods
    Add detailed doc comments to the `CurveOperations` trait, outlining the purposes and parameters of its methods: `generate_curve`, `interpolate`, and `analyze_curve`. This improves code readability and provides clear guidance for future developers.

--- src/curves/curve_traits.rs
+++ src/curves/curve_traits.rs
@@ -8,8 +8,41 @@ use crate::curves::interpolation::types::InterpolationType;
 use crate::curves::types::{Curve, CurveType};
 use std::error::Error;
 
+/// The `CurveOperations` trait defines a set of operations that can be performed on mathematical curves.
+///
+/// This trait includes methods for generating curves, interpolating values, and analyzing curves.
+/// Implementors of this trait are expected to provide concrete implementations for these methods.
+///
+/// # Methods
+///
+/// - `generate_curve(&self, x_values: Vec<f64>, curve_type: CurveType) -> Result<Curve, Box<dyn Error>>`
+///
+///   Generates a curve based on the provided x-values and the specified curve type.
+///
+///   - `x_values`: A vector of floating-point numbers representing the x-values of the curve.
+///   - `curve_type`: An enumeration indicating the type of curve to generate (e.g., linear, polynomial).
+///   - Returns: A result containing a `Curve` object on success or an error on failure.
+///
+/// - `interpolate(&self, x: f64, curve: &Curve, interpolation: InterpolationType) -> Option<f64>`
+///
+///   Interpolates a value on the given curve at the specified x-coordinate.
+///
+///   - `x`: The x-coordinate at which to interpolate.
+///   - `curve`: A reference to the curve on which interpolation is to be performed.
+///   - `interpolation`: The type of interpolation to use (e.g., linear, spline).
+///   - Returns: An optional floating-point number representing the interpolated y-value.
+///
+/// - `analyze_curve(&self, curve: &Curve) -> CurveAnalysisResult`
+///
+///   Analyzes the provided curve and returns a summary of the analysis.
+///
+///   - `curve`: A reference to the curve to be analyzed.
+///   - Returns: A `CurveAnalysisResult` containing the results of the analysis.
+///
 #[allow(dead_code)]
 pub trait CurveOperations {
+
+
     fn generate_curve(
         &self,
         x_values: Vec<f64>,
