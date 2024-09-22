eeb9953dc32cf578f6e56dea678734482568d099
JOAQUIN BEJAR <jb@taunais.com>
2024-09-22 19:10:40 +0100

    Format code for readability
    Aligned inline comments and adjusted multi-line formatting in `strategy_short_strangle.rs` to improve code readability and maintainability. Ensured consistent spacing and indentation for better visual clarity.

--- examples/strategy_short_strangle.rs
+++ examples/strategy_short_strangle.rs
@@ -13,20 +13,20 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let strategy = ShortStrangle::new(
         "GOLD".to_string(),
-        underlying_price,                    // underlying_price
-        2680.0,                    // call_strike
-        2620.0,                    // put_strike
+        underlying_price, // underlying_price
+        2680.0,           // call_strike
+        2620.0,           // put_strike
         ExpirationDate::Days(60.0),
-        0.1548,                      // implied_volatility
-        0.05,                     // risk_free_rate
-        0.0,                      // dividend_yield
-        1,                        // quantity
-        53.7,                      // premium_short_call
-        54.0,                      // premium_short_put
-        0.96,                      // open_fee_short_call
-        0.96,                      // close_fee_short_call
-        0.96,                      // open_fee_short_put
-        0.96,                      // close_fee_short_put
+        0.1548, // implied_volatility
+        0.05,   // risk_free_rate
+        0.0,    // dividend_yield
+        1,      // quantity
+        53.7,   // premium_short_call
+        54.0,   // premium_short_put
+        0.96,   // open_fee_short_call
+        0.96,   // close_fee_short_call
+        0.96,   // open_fee_short_put
+        0.96,   // close_fee_short_put
     );
 
     let price_range: Vec<f64> = (2450..=2850).map(|x| x as f64).collect();
@@ -38,7 +38,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     info!("Max Profit: {}", strategy.max_profit());
     info!("Max Loss: {}", strategy.max_loss());
     info!("Total Fees: {}", strategy.fees());
-    info!("Range of Profit: {:.2} {:.2}%", range, (range/2.0) / underlying_price * 100.0);
+    info!(
+        "Range of Profit: {:.2} {:.2}%",
+        range,
+        (range / 2.0) / underlying_price * 100.0
+    );
 
     // Generate the profit/loss graph
     strategy.graph(
