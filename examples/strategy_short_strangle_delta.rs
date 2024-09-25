3cb325dd5e32c89db77a2f92f49b9b553972d191
JOAQUIN BEJAR <jb@taunais.com>
2024-09-25 18:47:28 +0100

    Add short strangle delta strategy example
    Implemented a new example showcasing the short strangle delta strategy. This includes detailed logging of key metrics and outputs such as break-even points, net premium received, max profit, max loss, total fees, and profit range. Additionally, a generated graph visualizes the profit/loss over a specified price range.

---
+++ examples/strategy_short_strangle_delta.rs
@@ -0,0 +1,62 @@
+use optionstratlib::model::types::ExpirationDate;
+use optionstratlib::strategies::base::Strategies;
+use optionstratlib::strategies::strangle::ShortStrangle;
+use optionstratlib::utils::logger::setup_logger;
+use optionstratlib::visualization::utils::Graph;
+use std::error::Error;
+use tracing::info;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    setup_logger();
+
+    let underlying_price = 2655.6;
+
+    let strategy = ShortStrangle::new(
+        "GOLD".to_string(),
+        underlying_price, // underlying_price
+        2480.0,           // call_strike
+        2650.0,           // put_strike
+        ExpirationDate::Days(3.0),
+        0.1548, // implied_volatility
+        0.05,   // risk_free_rate
+        0.0,    // dividend_yield
+        2,      // quantity
+        46.3,   // premium_short_call
+        4.6,    // premium_short_put
+        0.96,   // open_fee_short_call
+        0.96,   // close_fee_short_call
+        0.96,   // open_fee_short_put
+        0.96,   // close_fee_short_put
+    );
+
+    let price_range: Vec<f64> = (2450..=2850).map(|x| x as f64).collect();
+    let range = strategy.break_even_points[1] - strategy.break_even_points[0];
+
+    info!("Title: {}", strategy.title());
+    info!("Break Even Points: {:?}", strategy.break_even_points);
+    info!(
+        "Net Premium Received: ${:.2}",
+        strategy.net_premium_received()
+    );
+    info!("Max Profit: ${:.2}", strategy.max_profit());
+    info!("Max Loss: ${}", strategy.max_loss());
+    info!("Total Fees: ${:.2}", strategy.fees());
+    info!(
+        "Range of Profit: ${:.2} {:.2}%",
+        range,
+        (range / 2.0) / underlying_price * 100.0
+    );
+    info!("Profit Area: {:.2}%", strategy.area());
+
+    // Generate the profit/loss graph
+    strategy.graph(
+        &price_range,
+        "Draws/Strategy/short_strangle_delta_profit_loss_chart.png",
+        20,
+        (1400, 933),
+        (10, 30),
+        15,
+    )?;
+
+    Ok(())
+}
