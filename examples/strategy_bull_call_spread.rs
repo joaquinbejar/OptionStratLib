2a8f36ee8ffea86d0ae61e79c01660c5bc3a7b13
JOAQUIN BEJAR <jb@taunais.com>
2024-09-28 08:55:09 +0100

    Add example strategy for Bull Call Spread
    Includes a detailed example for creating and analyzing a Bull Call Spread. The example logs essential details such as break-even points, max profit/loss, and generates a profit/loss graph.

---
+++ examples/strategy_bull_call_spread.rs
@@ -0,0 +1,68 @@
+/******************************************************************************
+   Author: Joaquín Béjar García
+   Email: jb@taunais.com
+   Date: 25/9/24
+******************************************************************************/
+use optionstratlib::model::types::ExpirationDate;
+use optionstratlib::strategies::base::Strategies;
+use optionstratlib::strategies::bull_call_spread::BullCallSpread;
+use optionstratlib::utils::logger::setup_logger;
+use optionstratlib::visualization::utils::Graph;
+use std::error::Error;
+use tracing::info;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    setup_logger();
+
+    let underlying_price = 5781.88;
+
+    let strategy = BullCallSpread::new(
+        "SP500".to_string(),
+        underlying_price, // underlying_price
+        5750.0,           // long_strike_itm
+        5820.0,           // short_strike
+        ExpirationDate::Days(2.0),
+        0.18,  // implied_volatility
+        0.05,  // risk_free_rate
+        0.0,   // dividend_yield
+        2,     // long quantity
+        85.04, // premium_long
+        29.85, // premium_short
+        0.78,  // open_fee_long
+        0.78,  // open_fee_long
+        0.73,  // close_fee_long
+        0.73,  // close_fee_short
+    );
+
+    let price_range = strategy.best_range_to_show(1.0).unwrap();
+    let range = strategy.break_even_points[1] - strategy.break_even_points[0];
+
+    info!("Title: {}", strategy.title());
+    info!("Break Even Points: {:?}", strategy.break_even_points);
+    info!(
+        "Net Premium Received: ${:.2}",
+        strategy.net_premium_received()
+    );
+    info!("Max Profit: ${:.2}", strategy.max_profit());
+    info!("Max Loss: ${:0.2}", strategy.max_loss());
+    info!("Total Fees: ${:.2}", strategy.fees());
+    info!(
+        "Range of Profit: ${:.2} {:.2}%",
+        range,
+        (range / 2.0) / underlying_price * 100.0
+    );
+    info!("Profit Area: {:.2}%", strategy.profit_area());
+    info!("Profit Ratio: {:.2}%", strategy.profit_ratio());
+
+    // Generate the profit/loss graph
+    strategy.graph(
+        &price_range,
+        "Draws/Strategy/bull_call_spread_profit_loss_chart.png",
+        20,
+        (1400, 933),
+        (10, 30),
+        15,
+    )?;
+
+    Ok(())
+}
