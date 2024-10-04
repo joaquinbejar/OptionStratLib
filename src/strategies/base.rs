3a51faf4e337ba91291ee792eb5674eba0ad3b76
Joaquin Bejar <jb@taunais.com>
2024-10-04 12:16:49 +0200

    Refactor types to use PositiveF64 for break-even points
    Updated methods and data structures to use PositiveF64 instead of f64 for break-even points, ensuring all newly added break-even points are valid and positive. This change aims to improve the reliability and accuracy of calculations throughout the trading strategy module.

--- src/strategies/base.rs
+++ src/strategies/base.rs
@@ -7,12 +7,13 @@
 use crate::constants::ZERO;
 use crate::model::chain::OptionChain;
 use crate::model::position::Position;
-use crate::model::types::Side;
+use crate::model::types::{PositiveF64, Side, PZERO};
 use crate::strategies::utils::FindOptimalSide;
 use crate::visualization::model::ChartVerticalLine;
 use crate::visualization::utils::Graph;
 use num_traits::Float;
 use plotters::prelude::{ShapeStyle, BLACK};
+use crate::spos;
 
 /// This enum represents different types of trading strategies.
 /// Each variant represents a specific strategy type.
@@ -55,7 +56,7 @@ pub struct Strategy {
     pub legs: Vec<Position>,
     pub max_profit: Option<f64>,
     pub max_loss: Option<f64>,
-    pub break_even_points: Vec<f64>,
+    pub break_even_points: Vec<PositiveF64>,
 }
 
 impl Strategy {
@@ -71,78 +72,32 @@ impl Strategy {
         }
     }
 
-    pub fn add_leg(&mut self, position: Position) {
-        self.legs.push(position);
-    }
-
-    pub fn set_max_profit(&mut self, max_profit: f64) {
-        self.max_profit = Some(max_profit);
-    }
-
-    pub fn set_max_loss(&mut self, max_loss: f64) {
-        self.max_loss = Some(max_loss);
-    }
-
-    pub fn add_break_even_point(&mut self, point: f64) {
-        self.break_even_points.push(point);
-    }
-
-    pub fn break_even(&self) -> f64 {
-        match self.kind {
-            StrategyType::BullCallSpread => {
-                let long_call = self
-                    .legs
-                    .iter()
-                    .find(|leg| leg.option.side == Side::Long)
-                    .unwrap();
-                let short_call = self
-                    .legs
-                    .iter()
-                    .find(|leg| leg.option.side == Side::Short)
-                    .unwrap();
-                long_call.option.strike_price
-                    + (long_call.total_cost() - short_call.net_premium_received())
-                        / long_call.option.quantity
-            }
-            StrategyType::BearCallSpread => ZERO,
-            StrategyType::BullPutSpread => ZERO,
-            StrategyType::BearPutSpread => ZERO,
-            StrategyType::IronCondor => ZERO,
-            StrategyType::Straddle => ZERO,
-            StrategyType::Strangle => ZERO,
-            StrategyType::CoveredCall => ZERO,
-            StrategyType::ProtectivePut => ZERO,
-            StrategyType::Collar => ZERO,
-            StrategyType::LongCall => ZERO,
-            StrategyType::LongPut => ZERO,
-            StrategyType::ShortCall => ZERO,
-            StrategyType::ShortPut => ZERO,
-            StrategyType::PoorMansCoveredCall => {
-                let long_call = self
-                    .legs
-                    .iter()
-                    .find(|leg| leg.option.side == Side::Long)
-                    .unwrap();
-                let short_call = self
-                    .legs
-                    .iter()
-                    .find(|leg| leg.option.side == Side::Short)
-                    .unwrap();
-                let net_debit =
-                    (long_call.max_loss() - short_call.max_profit()) / long_call.option.quantity;
-                long_call.option.strike_price + net_debit
-            }
-            StrategyType::CallButterfly => ZERO,
-            StrategyType::Custom => ZERO,
-        }
-    }
-
-    pub fn calculate_profit_at(&self, price: f64) -> f64 {
-        self.legs
-            .iter()
-            .map(|leg| leg.pnl_at_expiration(Some(price)))
-            .sum()
-    }
+    // pub fn add_leg(&mut self, position: Position) {
+    //     self.legs.push(position);
+    // }
+    //
+    // pub fn set_max_profit(&mut self, max_profit: f64) {
+    //     self.max_profit = Some(max_profit);
+    // }
+    //
+    // pub fn set_max_loss(&mut self, max_loss: f64) {
+    //     self.max_loss = Some(max_loss);
+    // }
+    //
+    // pub fn add_break_even_point(&mut self, point: PositiveF64) {
+    //     self.break_even_points.push(point);
+    // }
+    //
+    // pub fn break_even(&self) -> Vec<PositiveF64> {
+    //     vec![]
+    // }
+    //
+    // pub fn calculate_profit_at(&self, price: PositiveF64) -> f64 {
+    //     self.legs
+    //         .iter()
+    //         .map(|leg| leg.pnl_at_expiration(Some(price)))
+    //         .sum()
+    // }
 }
 
 impl Graph for Strategy {
@@ -157,15 +112,15 @@ impl Graph for Strategy {
         }
     }
 
-    fn get_values(&self, data: &[f64]) -> Vec<f64> {
+    fn get_values<T>(&self, data: &[T]) -> Vec<f64> {
         data.iter()
             .map(|&price| self.calculate_profit_at(price))
             .collect()
     }
 
-    fn get_vertical_lines(&self) -> Vec<ChartVerticalLine<f64, f64>> {
+    fn get_vertical_lines(&self) -> Vec<ChartVerticalLine<PositiveF64, f64>> {
         let vertical_lines = vec![ChartVerticalLine {
-            x_coordinate: self.break_even(),
+            x_coordinate: PZERO,
             y_range: (-50000.0, 50000.0),
             label: "Break Even".to_string(),
             label_offset: (5.0, 5.0),
@@ -182,9 +137,9 @@ impl Graph for Strategy {
 pub trait Strategies {
     fn add_leg(&mut self, position: Position);
 
-    fn break_even(&self) -> f64;
+    fn break_even(&self) -> Vec<PositiveF64>;
 
-    fn calculate_profit_at(&self, price: f64) -> f64;
+    fn calculate_profit_at(&self, price: PositiveF64) -> f64;
 
     fn max_profit(&self) -> f64;
 
@@ -216,7 +171,7 @@ pub trait Strategies {
         true
     }
 
-    fn best_range_to_show(&self, _step: f64) -> Option<Vec<f64>> {
+    fn best_range_to_show(&self, _step: PositiveF64) -> Option<Vec<PositiveF64>> {
         None
     }
 }
