bb8401e41ad4d53c9dfc2aed47e6bb66df1823e2
Joaquin Bejar <jb@taunais.com>
2024-10-05 11:49:15 +0200

    Add utility to convert PositiveF64 to f64 and its tests
    Implemented a function `positive_f64_to_f64` to convert a vector of `PositiveF64` to `f64`. Added unit tests to ensure the function works with non-empty vectors, single elements, and handles invalid `PositiveF64` creation appropriately.

--- src/model/utils.rs
+++ src/model/utils.rs
@@ -9,6 +9,12 @@ use crate::pos;
 use chrono::{NaiveDateTime, TimeZone, Utc};
 
 #[allow(dead_code)]
+pub  fn positive_f64_to_f64(vec: Vec<PositiveF64>) -> Vec<f64> {
+    vec.into_iter().map(|pos_f64| pos_f64.value()).collect()
+}
+
+
+#[allow(dead_code)]
 pub(crate) fn create_sample_option(
     option_style: OptionStyle,
     side: Side,
@@ -97,4 +103,39 @@ pub(crate) fn create_sample_option_simplest_strike(
         0.01,
         None,
     )
+}
+
+#[cfg(test)]
+mod tests_positive_f64_to_f64 {
+    use super::*;
+
+    #[test]
+    fn test_positive_f64_to_f64_non_empty() {
+        let positive_vec = vec![
+            PositiveF64::new(10.0).unwrap(),
+            PositiveF64::new(20.0).unwrap(),
+            PositiveF64::new(30.0).unwrap(),
+        ];
+
+        let f64_vec = positive_f64_to_f64(positive_vec);
+
+        assert_eq!(f64_vec, vec![10.0, 20.0, 30.0]);
+    }
+
+    #[test]
+    fn test_positive_f64_to_f64_single_element() {
+        let positive_vec = vec![PositiveF64::new(42.0).unwrap()];
+
+        let f64_vec = positive_f64_to_f64(positive_vec);
+
+        assert_eq!(f64_vec, vec![42.0]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_positive_f64_to_f64_invalid_positivef64() {
+        // Esto provocará un panic ya que estamos intentando crear un `PositiveF64` con un valor negativo
+        PositiveF64::new(-10.0).unwrap();
+    }
+
 }
