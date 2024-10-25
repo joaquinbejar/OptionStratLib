2dcbff905e602ede5c05af996f38d4112b58b362
Joaquin Bejar <jb@taunais.com>
2024-10-25 12:55:31 +0200

    Refactor import path for OptionChain and OptionData
    Updated the import path from crate::model::chain to crate::chains::chain for better package structure. This change ensures that all option chain related modules are grouped under a single directory, improving code maintainability and readability.

--- src/strategies/bull_call_spread.rs
+++ src/strategies/bull_call_spread.rs
@@ -13,7 +13,7 @@ use super::base::{Strategies, StrategyType};
 use crate::constants::{
     DARK_BLUE, DARK_GREEN, STRIKE_PRICE_LOWER_BOUND_MULTIPLIER, STRIKE_PRICE_UPPER_BOUND_MULTIPLIER,
 };
-use crate::model::chain::{OptionChain, OptionData};
+use crate::chains::chain::{OptionChain, OptionData};
 use crate::model::option::Options;
 use crate::model::position::Position;
 use crate::model::types::{ExpirationDate, OptionStyle, OptionType, PositiveF64, Side, PZERO};
