69a28eaafcbdb1ae3ddd33a183daa72a4fd2daaa
Joaquin Bejar <jb@taunais.com>
2024-10-27 20:46:50 +0100

    Refactor random_walk example to enhance simulation
    Updated the random_walk example to include new parameters such as risk-free rate, dividend yield, and initial volatility. Added logging for each step in the random walk to facilitate better debugging and analysis.

--- examples/random_walk.rs
+++ examples/random_walk.rs
@@ -7,17 +7,35 @@ use optionstratlib::model::types::PositiveF64;
 use optionstratlib::pos;
 use optionstratlib::simulation::walk::{RandomWalkGraph, Walkable};
 use optionstratlib::utils::logger::setup_logger;
+use optionstratlib::utils::time::TimeFrame;
 use optionstratlib::visualization::utils::Graph;
+use tracing::info;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     setup_logger();
-    let n_steps = 1000;
+    let years = 3.0;
+    let n_steps = 252 * years as usize;
     let initial_price = pos!(100.0);
     let mean = 0.02;
     let std_dev = pos!(1.0);
     let std_dev_change = pos!(0.1);
-
-    let mut random_walk = RandomWalkGraph::new("Random Walk".to_string());
+    let risk_free_rate = Some(0.05);
+    let dividend_yield = Some(0.02);
+    let volatility_window = 20;
+    let initial_volatility = Some(std_dev);
+    let mut random_walk = RandomWalkGraph::new(
+        "Random Walk".to_string(),
+        risk_free_rate,
+        dividend_yield,
+        TimeFrame::Day,
+        volatility_window,
+        initial_volatility,
+    );
     random_walk.generate_random_walk(n_steps, initial_price, mean, std_dev, std_dev_change);
-    random_walk.graph(&[], "Draws/Simulation/random_walk.png", 20, (1200, 800))
+    let _ = random_walk.graph(&[], "Draws/Simulation/random_walk.png", 20, (1200, 800));
+
+    for (i, params) in random_walk.enumerate() {
+        info!("Step {}: Params: {}", i, params,);
+    }
+    Ok(())
 }
