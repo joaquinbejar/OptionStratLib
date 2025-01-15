fb726f5d3a9c21355aa0262d16d6b9b0f7339dfa
Joaquin Bejar <jb@taunais.com>
2025-01-15 20:37:40 +0100

    Update ShortStrangle test to use DELTA_THRESHOLD and fix values
    Replaced hardcoded epsilon with DELTA_THRESHOLD for consistency and updated test input/output values to align with the new threshold. Adjustments improve accuracy in asserting `greeks` and delta neutrality calculations. (ref. #105)

--- tests/unit/strategies/delta/strategy_short_strangle.rs
+++ tests/unit/strategies/delta/strategy_short_strangle.rs
@@ -1,20 +1,20 @@
-use approx::assert_relative_eq;
 use optionstratlib::greeks::Greeks;
 use optionstratlib::strategies::delta_neutral::DeltaAdjustment::NoAdjustmentNeeded;
 use optionstratlib::strategies::delta_neutral::DeltaNeutrality;
-use optionstratlib::strategies::ShortStrangle;
+use optionstratlib::strategies::{ShortStrangle, DELTA_THRESHOLD};
 use optionstratlib::utils::setup_logger;
 use optionstratlib::{assert_decimal_eq, pos};
 use optionstratlib::{ExpirationDate, Positive};
 use rust_decimal_macros::dec;
 use std::error::Error;
+use rust_decimal::Decimal;
 
 #[test]
 fn test_short_strangle_with_greeks_integration() -> Result<(), Box<dyn Error>> {
     setup_logger();
 
     // Define inputs for the ShortStrangle strategy
-    let underlying_price = pos!(7138.5);
+    let underlying_price = pos!(7140.6);
 
     let strategy = ShortStrangle::new(
         "CL".to_string(),
@@ -35,29 +35,29 @@ fn test_short_strangle_with_greeks_integration() -> Result<(), Box<dyn Error>> {
     );
 
     let greeks = strategy.greeks().unwrap();
-    let epsilon = dec!(0.001);
+    let epsilon = DELTA_THRESHOLD;
 
-    assert_decimal_eq!(greeks.delta, dec!(0.0018), epsilon);
+    assert_decimal_eq!(greeks.delta, dec!(0.00001), epsilon);
     assert_decimal_eq!(greeks.gamma, dec!(0.0008), epsilon);
-    assert_decimal_eq!(greeks.theta, dec!(-2942.0709), epsilon);
-    assert_decimal_eq!(greeks.vega, dec!(2501.9092), epsilon);
-    assert_decimal_eq!(greeks.rho, dec!(-72.0661), epsilon);
-    assert_decimal_eq!(greeks.rho_d, dec!(1.6100), epsilon);
+    assert_decimal_eq!(greeks.theta, dec!(-2943.57608), epsilon);
+    assert_decimal_eq!(greeks.vega, dec!(2507.0226386), epsilon);
+    assert_decimal_eq!(greeks.rho, dec!(-70.52940734), epsilon);
+    assert_decimal_eq!(greeks.rho_d, dec!(0.07352819), epsilon);
 
-    assert_relative_eq!(
+    assert_decimal_eq!(
         strategy.calculate_net_delta().net_delta,
-        0.0018,
-        epsilon = 0.001
+        Decimal::ZERO,
+        DELTA_THRESHOLD
     );
-    assert_relative_eq!(
+    assert_decimal_eq!(
         strategy.calculate_net_delta().individual_deltas[0],
-        -0.4159,
-        epsilon = 0.001
+        dec!(-0.4168540),
+        DELTA_THRESHOLD
     );
-    assert_relative_eq!(
+    assert_decimal_eq!(
         strategy.calculate_net_delta().individual_deltas[1],
-        0.4178,
-        epsilon = 0.001
+        dec!(0.4169376),
+        DELTA_THRESHOLD
     );
     assert!(strategy.is_delta_neutral());
     assert_eq!(strategy.suggest_delta_adjustments().len(), 1);
