e773ceb77d187aa4712b0e2386dd5824bdfa431d
Joaquin Bejar <jb@taunais.com>
2025-01-16 08:43:15 +0100

    Refactor assert statements in bull_put_spread tests
    Replace `assert_relative_eq!` with `assert_decimal_eq!` to improve clarity and align with updated library standards. Added `DELTA_THRESHOLD` for consistent precision handling and restructured delta adjustment validation for better readability. (ref. #105)

--- tests/unit/strategies/delta/strategy_bull_put_spread.rs
+++ tests/unit/strategies/delta/strategy_bull_put_spread.rs
@@ -1,13 +1,13 @@
-use approx::assert_relative_eq;
 use optionstratlib::greeks::Greeks;
 use optionstratlib::model::types::{ExpirationDate, OptionStyle};
 use optionstratlib::strategies::bull_put_spread::BullPutSpread;
 use optionstratlib::strategies::delta_neutral::DeltaAdjustment::BuyOptions;
 use optionstratlib::strategies::delta_neutral::DeltaNeutrality;
 use optionstratlib::utils::setup_logger;
-use optionstratlib::{assert_decimal_eq, pos, Positive};
+use optionstratlib::{assert_decimal_eq, assert_pos_relative_eq, pos, Positive};
 use rust_decimal_macros::dec;
 use std::error::Error;
+use optionstratlib::strategies::DELTA_THRESHOLD;
 
 #[test]
 fn test_bull_put_spread_integration() -> Result<(), Box<dyn Error>> {
@@ -44,32 +44,34 @@ fn test_bull_put_spread_integration() -> Result<(), Box<dyn Error>> {
     assert_decimal_eq!(greeks.rho, dec!(-83.3461), epsilon);
     assert_decimal_eq!(greeks.rho_d, dec!(81.6525), epsilon);
 
-    assert_relative_eq!(
+    assert_decimal_eq!(
         strategy.calculate_net_delta().net_delta,
-        1.2605,
-        epsilon = 0.001
+        dec!(1.2605),
+        DELTA_THRESHOLD
     );
-    assert_relative_eq!(
+    assert_decimal_eq!(
         strategy.calculate_net_delta().individual_deltas[0],
-        1.9189,
-        epsilon = 0.001
+        dec!(1.9189),
+        DELTA_THRESHOLD
     );
-    assert_relative_eq!(
+    assert_decimal_eq!(
         strategy.calculate_net_delta().individual_deltas[1],
-        -0.6583,
-        epsilon = 0.001
+        dec!(-0.6583),
+        DELTA_THRESHOLD
     );
     assert!(!strategy.is_delta_neutral());
     assert_eq!(strategy.suggest_delta_adjustments().len(), 1);
-
-    assert_eq!(
-        strategy.suggest_delta_adjustments()[0],
-        BuyOptions {
-            quantity: pos!(3.829496711654006),
-            strike: pos!(5750.0),
-            option_type: OptionStyle::Put
-        }
-    );
-
+    let binding = strategy.suggest_delta_adjustments();
+    let suggestion = binding.first().unwrap();
+    let delta = pos!(3.829496711654006);
+    let k = pos!(5750.0);
+    match suggestion {
+        BuyOptions { quantity, strike, option_type } => {
+            assert_pos_relative_eq!(*quantity, delta, Positive::new_decimal(DELTA_THRESHOLD).unwrap());
+            assert_pos_relative_eq!(*strike, k, Positive::new_decimal(DELTA_THRESHOLD).unwrap());
+            assert_eq!(*option_type, OptionStyle::Put);
+        },
+        _ => panic!("Invalid suggestion")
+    }
     Ok(())
 }
