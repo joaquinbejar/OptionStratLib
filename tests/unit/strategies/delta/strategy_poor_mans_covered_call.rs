7dd2c27fad74af81491db8c4893d15016ef201ea
Joaquin Bejar <jb@taunais.com>
2025-01-16 08:35:36 +0100

    Refactor delta assertions in Poor Man's Covered Call test
    Replaced `assert_relative_eq!` with `assert_decimal_eq!` and standardized epsilon usage by introducing `DELTA_THRESHOLD`. Improved the `suggest_delta_adjustments` validation by expanding checks with `assert_pos_relative_eq!` for better precision. (ref. #105)

--- tests/unit/strategies/delta/strategy_poor_mans_covered_call.rs
+++ tests/unit/strategies/delta/strategy_poor_mans_covered_call.rs
@@ -1,15 +1,13 @@
-use approx::assert_relative_eq;
 use optionstratlib::greeks::Greeks;
 use optionstratlib::model::types::{ExpirationDate, OptionStyle};
 use optionstratlib::strategies::delta_neutral::DeltaAdjustment::SellOptions;
 use optionstratlib::strategies::delta_neutral::DeltaNeutrality;
 use optionstratlib::strategies::poor_mans_covered_call::PoorMansCoveredCall;
 use optionstratlib::utils::setup_logger;
-use optionstratlib::{assert_decimal_eq, pos, Positive};
-use rust_decimal::Decimal;
+use optionstratlib::{assert_decimal_eq, assert_pos_relative_eq, pos, Positive};
 use rust_decimal_macros::dec;
 use std::error::Error;
-use std::str::FromStr;
+use optionstratlib::strategies::DELTA_THRESHOLD;
 
 #[test]
 fn test_poor_mans_covered_call_integration() -> Result<(), Box<dyn Error>> {
@@ -46,33 +44,36 @@ fn test_poor_mans_covered_call_integration() -> Result<(), Box<dyn Error>> {
     assert_decimal_eq!(greeks.rho, dec!(1290.9435), epsilon);
     assert_decimal_eq!(greeks.rho_d, dec!(-1420.1310), epsilon);
 
-    assert_relative_eq!(
+    assert_decimal_eq!(
         strategy.calculate_net_delta().net_delta,
-        0.9225,
-        epsilon = 0.001
+        dec!(0.9225),
+        DELTA_THRESHOLD
     );
-    assert_relative_eq!(
+    assert_decimal_eq!(
         strategy.calculate_net_delta().individual_deltas[0],
-        1.4628,
-        epsilon = 0.001
+        dec!(1.4628),
+        DELTA_THRESHOLD
     );
-    assert_relative_eq!(
+    assert_decimal_eq!(
         strategy.calculate_net_delta().individual_deltas[1],
-        -0.5402,
-        epsilon = 0.001
+        dec!(-0.5402),
+        DELTA_THRESHOLD
     );
     assert!(!strategy.is_delta_neutral());
     assert_eq!(strategy.suggest_delta_adjustments().len(), 1);
+    let binding = strategy.suggest_delta_adjustments();
+    let suggestion = binding.first().unwrap();
+    let delta = pos!(3.415412207592464);
+    let k = pos!(2800.0);
+    match suggestion {
+        SellOptions { quantity, strike, option_type } => {
+            assert_pos_relative_eq!(*quantity, delta, Positive::new_decimal(DELTA_THRESHOLD).unwrap());
+            assert_pos_relative_eq!(*strike, k, Positive::new_decimal(DELTA_THRESHOLD).unwrap());
+            assert_eq!(*option_type, OptionStyle::Call);
+        },
+        _ => panic!("Invalid suggestion")
+    }
 
-    assert_eq!(
-        strategy.suggest_delta_adjustments()[0],
-        SellOptions {
-            quantity: Positive::new_decimal(Decimal::from_str("3.415412207592464").unwrap())
-                .unwrap(),
-            strike: pos!(2800.0),
-            option_type: OptionStyle::Call
-        }
-    );
 
     Ok(())
 }
