23588455fa09da6bc621601bfb8ff77ead976794
Joaquin Bejar <jb@taunais.com>
2025-01-30 10:08:48 +0100

    Refactor example and update DAX-30 options data
    Refactored import structure for better clarity and usage in `option_chain_raw.rs`. Updated option chain data for DAX-30 with adjusted underlying price, expiration date, and bid/ask values. Simplified strategy implementation and commented obsolete call for best area calculation.

--- examples/examples_chain/src/bin/option_chain_raw.rs
+++ examples/examples_chain/src/bin/option_chain_raw.rs
@@ -7,11 +7,13 @@ use rust_decimal::Decimal;
 use optionstratlib::chains::chain::OptionChain;
 use optionstratlib::utils::setup_logger;
 use tracing::{debug, info};
-use optionstratlib::{pos, ExpirationDate, Positive};
+use optionstratlib::{pos, Positive};
+use optionstratlib::ExpirationDate;
 use optionstratlib::greeks::Greeks;
 use optionstratlib::strategies::{FindOptimalSide, ShortStrangle, Strategies};
 use optionstratlib::strategies::base::Optimizable;
 use optionstratlib::visualization::utils::{Graph, GraphBackend};
+use chrono::DateTime;
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     setup_logger();
@@ -20,14 +22,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     option_chain.update_deltas();
     println!("{}", &option_chain);
 
+    let datetime = DateTime::parse_from_rfc3339("2025-01-30T18:30:00Z").unwrap();
 
     let mut strategy = ShortStrangle::new(
         "SP500".to_string(),
         option_chain.underlying_price, // underlying_price
         Positive::ZERO,   // call_strike
         Positive::ZERO,   // put_strike
-        ExpirationDate::Days(pos!(1.0)),
-        Positive::ZERO, // implied_volatility
+        ExpirationDate::DateTime(DateTime::from(datetime)),
+        Positive::ONE, // implied_volatility
         Decimal::ZERO,  // risk_free_rate
         Positive::ZERO, // dividend_yield
         pos!(1.0),      // quantity
@@ -39,7 +42,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
         pos!(1.7),     // close_fee_short_put
     );
 
-    strategy.best_area(&option_chain, FindOptimalSide::Range(pos!(21600.0), pos!(21700.0) ));
+    // strategy.best_area(&option_chain, FindOptimalSide::Range(pos!(21600.0), pos!(21700.0) ));
+    strategy.best_area(&option_chain, FindOptimalSide::Upper);
     debug!("Strategy:  {:#?}", strategy);
     let price_range = strategy.best_range_to_show(pos!(1.0)).unwrap();
     let range = strategy.range_of_profit().unwrap_or(Positive::ZERO);
