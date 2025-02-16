b0493198d14c3a8885b64121a8d4beb48484c769
Joaquin Bejar <jb@taunais.com>
2025-02-16 19:03:18 +0100

    Refactor strategy constructor for better error propagation
    Updated `get_strategy` methods to propagate errors using the `?` operator. This ensures more concise and idiomatic error handling. Added a `where Self: Sized` constraint to the trait implementation for improved type safety.

--- src/strategies/general.rs
+++ src/strategies/general.rs
@@ -25,7 +25,7 @@ pub struct StrategyRequest {
 }
 
 pub trait StrategyConstructor: Strategies {
-    fn get_strategy( _vec_options: &Vec<OptionWithCosts>) -> Result<Self, StrategyError> {
+    fn get_strategy( _vec_options: &Vec<OptionWithCosts>) -> Result<Self, StrategyError> where Self: Sized {
         Err(StrategyError::NotImplemented)
     }
 }
@@ -40,18 +40,18 @@ impl StrategyRequest {
     
     pub fn get_strategy(&self) -> Result<Box<dyn Strategies>, StrategyError> {
         match self.strategy_type {
-            StrategyType::BullCallSpread => Ok(Box::new(BullCallSpread::get_strategy(&self.options))),
-            StrategyType::BearCallSpread => Ok(Box::new(BearCallSpread::get_strategy(&self.options))),
-            StrategyType::BullPutSpread => Ok(Box::new(BullPutSpread::get_strategy(&self.options))),
-            StrategyType::BearPutSpread => Ok(Box::new(BearPutSpread::get_strategy(&self.options))),
-            StrategyType::LongButterflySpread => Ok(Box::new(LongButterflySpread::get_strategy(&self.options))),
-            StrategyType::ShortButterflySpread => Ok(Box::new(ShortButterflySpread::get_strategy(&self.options))),
-            StrategyType::IronCondor => Ok(Box::new(IronCondor::get_strategy(&self.options))),
-            StrategyType::IronButterfly => Ok(Box::new(IronButterfly::get_strategy(&self.options))),
-            StrategyType::LongStraddle => Ok(Box::new(LongStraddle::get_strategy(&self.options))),
-            StrategyType::ShortStraddle => Ok(Box::new(ShortStraddle::get_strategy(&self.options))),
-            StrategyType::LongStrangle => Ok(Box::new(LongStrangle::get_strategy(&self.options))),
-            StrategyType::ShortStrangle => Ok(Box::new(ShortStrangle::get_strategy(&self.options))),
+            StrategyType::BullCallSpread => Ok(Box::new(BullCallSpread::get_strategy(&self.options)?)),
+            StrategyType::BearCallSpread => Ok(Box::new(BearCallSpread::get_strategy(&self.options)?)),
+            StrategyType::BullPutSpread => Ok(Box::new(BullPutSpread::get_strategy(&self.options)?)),
+            StrategyType::BearPutSpread => Ok(Box::new(BearPutSpread::get_strategy(&self.options)?)),
+            StrategyType::LongButterflySpread => Ok(Box::new(LongButterflySpread::get_strategy(&self.options)?)),
+            StrategyType::ShortButterflySpread => Ok(Box::new(ShortButterflySpread::get_strategy(&self.options)?)),
+            StrategyType::IronCondor => Ok(Box::new(IronCondor::get_strategy(&self.options)?)),
+            StrategyType::IronButterfly => Ok(Box::new(IronButterfly::get_strategy(&self.options)?)),
+            StrategyType::LongStraddle => Ok(Box::new(LongStraddle::get_strategy(&self.options)?)),
+            StrategyType::ShortStraddle => Ok(Box::new(ShortStraddle::get_strategy(&self.options)?)),
+            StrategyType::LongStrangle => Ok(Box::new(LongStrangle::get_strategy(&self.options)?)),
+            StrategyType::ShortStrangle => Ok(Box::new(ShortStrangle::get_strategy(&self.options)?)),
             StrategyType::CoveredCall => Err(StrategyError::NotImplemented),
             StrategyType::ProtectivePut => Err(StrategyError::NotImplemented),
             StrategyType::Collar => Err(StrategyError::NotImplemented),
@@ -59,9 +59,9 @@ impl StrategyRequest {
             StrategyType::LongPut => Err(StrategyError::NotImplemented),
             StrategyType::ShortCall =>Err(StrategyError::NotImplemented),
             StrategyType::ShortPut => Err(StrategyError::NotImplemented),
-            StrategyType::PoorMansCoveredCall => Ok(Box::new(PoorMansCoveredCall::get_strategy(&self.options))),
-            StrategyType::CallButterfly => Ok(Box::new(CallButterfly::get_strategy(&self.options))),
-            StrategyType::Custom => Ok(Box::new(CustomStrategy::get_strategy(&self.options))),
+            StrategyType::PoorMansCoveredCall => Ok(Box::new(PoorMansCoveredCall::get_strategy(&self.options)?)),
+            StrategyType::CallButterfly => Ok(Box::new(CallButterfly::get_strategy(&self.options)?)),
+            StrategyType::Custom => Ok(Box::new(CustomStrategy::get_strategy(&self.options)?)),
         }
     }
 }
