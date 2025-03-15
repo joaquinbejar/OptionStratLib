37ee656373d03cedbdf9becc4f620767c2a7e467
Joaquin Bejar <jb@taunais.com>
2025-03-15 17:09:16 +0100

    Add unit test for IronCondor strategy integration
    This test verifies the IronCondor strategy's ability to calculate optimal positions based on an SP500 options chain. It ensures positions align with expected strike prices relative to the underlying price and confirms proper strategy behavior.

---
+++ tests/unit/strategies/optimal_center/strategy_iron_condor.rs
@@ -0,0 +1,64 @@
+use optionstratlib::strategies::base::Positionable;
+use optionstratlib::{OptionStyle, Side};
+#[cfg(not(target_arch = "wasm32"))]
+use {
+    optionstratlib::chains::chain::OptionChain,
+    optionstratlib::strategies::base::Optimizable,
+    optionstratlib::strategies::{FindOptimalSide, IronCondor},
+    optionstratlib::utils::setup_logger,
+    optionstratlib::{ExpirationDate, Positive, pos},
+    rust_decimal_macros::dec,
+    std::error::Error,
+};
+
+#[test]
+#[cfg(not(target_arch = "wasm32"))]
+fn test_iron_condor_integration() -> Result<(), Box<dyn Error>> {
+    setup_logger();
+
+    // Define inputs for the IronCondor strategy
+    let underlying_price = pos!(2646.9);
+
+    let mut strategy = IronCondor::new(
+        "GOLD".to_string(),
+        underlying_price, // underlying_price
+        pos!(2725.0),     // short_call_strike
+        pos!(2560.0),     // short_put_strike
+        pos!(2800.0),     // long_call_strike
+        pos!(2500.0),     // long_put_strike
+        ExpirationDate::Days(pos!(30.0)),
+        pos!(0.1548),   // implied_volatility
+        dec!(0.05),     // risk_free_rate
+        Positive::ZERO, // dividend_yield
+        pos!(2.0),      // quantity
+        pos!(38.8),     // premium_short_call
+        pos!(30.4),     // premium_short_put
+        pos!(23.3),     // premium_long_call
+        pos!(16.8),     // premium_long_put
+        pos!(0.96),     // open_fee
+        pos!(0.96),     // close_fee
+    );
+
+    let option_chain =
+        OptionChain::load_from_json("./examples/Chains/SP500-18-oct-2024-5781.88.json")?;
+    strategy.best_area(&option_chain, FindOptimalSide::Center);
+    let positions = strategy.get_positions()?;
+    for position in positions {
+        match (position.option.option_style, position.option.side) {
+            (OptionStyle::Call, Side::Long) => {
+                assert!(position.option.strike_price >= underlying_price)
+            }
+            (OptionStyle::Put, Side::Long) => {
+                assert!(position.option.strike_price <= underlying_price)
+            }
+            (OptionStyle::Call, Side::Short) => {
+                assert!(position.option.strike_price >= underlying_price)
+            }
+            (OptionStyle::Put, Side::Short) => {
+                assert!(position.option.strike_price <= underlying_price)
+            }
+        }
+    }
+
+    Ok(())
+}
