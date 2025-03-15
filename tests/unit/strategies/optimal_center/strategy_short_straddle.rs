bc86fe72298b71f7acab6e10948216b2958c6476
Joaquin Bejar <jb@taunais.com>
2025-03-15 18:25:50 +0100

    Add unit tests for ShortStraddle and LongStraddle strategies
    Introduce integration tests for ShortStraddle and LongStraddle strategies to validate their behavior with given inputs. Tests ensure accuracy in position creation and optimization around the at-the-money strike.

---
+++ tests/unit/strategies/optimal_center/strategy_short_straddle.rs
@@ -0,0 +1,56 @@
+#[cfg(not(target_arch = "wasm32"))]
+use {
+    optionstratlib::chains::chain::OptionChain,
+    optionstratlib::strategies::base::Optimizable,
+    optionstratlib::strategies::{FindOptimalSide, ShortStraddle},
+    optionstratlib::utils::setup_logger,
+    optionstratlib::{ExpirationDate, Positive, pos},
+    rust_decimal_macros::dec,
+    std::error::Error,
+};
+use optionstratlib::OptionStyle;
+use optionstratlib::strategies::base::Positionable;
+
+#[test]
+#[cfg(not(target_arch = "wasm32"))]
+fn test_short_straddle_integration() -> Result<(), Box<dyn Error>> {
+    setup_logger();
+
+    // Define inputs for the ShortStraddle strategy
+    let underlying_price = pos!(7138.5);
+
+    let mut strategy = ShortStraddle::new(
+        "CL".to_string(),
+        underlying_price, // underlying_price
+        pos!(7140.0),     // put_strike
+        ExpirationDate::Days(pos!(45.0)),
+        pos!(0.3745),   // implied_volatility
+        dec!(0.05),     // risk_free_rate
+        Positive::ZERO, // dividend_yield
+        pos!(1.0),      // quantity
+        pos!(84.2),     // premium_short_call
+        pos!(353.2),    // premium_short_put
+        pos!(7.01),     // open_fee_short_call
+        pos!(7.01),     // close_fee_short_call
+        pos!(7.01),     // open_fee_short_put
+        pos!(7.01),     // close_fee_short_put
+    );
+
+    let option_chain =
+        OptionChain::load_from_json("./examples/Chains/SP500-18-oct-2024-5781.88.json")?;
+    strategy.best_area(&option_chain, FindOptimalSide::Center);
+    let positions = strategy.get_positions()?;
+    let atm_strike =  option_chain.atm_strike()?;
+    for position in positions {
+        match position.option.option_style {
+            OptionStyle::Call => {
+                assert!(position.option.strike_price == atm_strike)
+            },
+            OptionStyle::Put => {
+                assert!(position.option.strike_price == atm_strike)
+            },
+        }
+    }
+
+    Ok(())
+}
