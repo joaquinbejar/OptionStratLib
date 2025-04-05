ab130d50990f9ea432f9db417031db5e9c5164ce
Joaquin Bejar <jb@taunais.com>
2025-04-05 10:15:26 +0200

    Refactor volatility calculation in delta hedging example
    Extract volatility computation into a dedicated function `get_volatilities_from_ohlcv` for clarity and reusability. This improves maintainability and prepares the codebase for additional enhancements. Also, adjust `read_ohlcv_from_zip` calls to handle optional date parameters.

--- examples/examples_simulation/src/bin/delta_hedging.rs
+++ examples/examples_simulation/src/bin/delta_hedging.rs
@@ -1,21 +1,58 @@
+use std::process::exit;
 use optionstratlib::chains::OptionChain;
 use optionstratlib::chains::utils::{OptionChainBuildParams, OptionDataPriceParams};
 use optionstratlib::pnl::{PnL, PnLCalculator, PnLMetricsStep};
 use optionstratlib::strategies::base::{Optimizable, Positionable};
 use optionstratlib::strategies::{FindOptimalSide, ShortStrangle};
-use optionstratlib::utils::{read_ohlcv_from_zip, setup_logger};
+use optionstratlib::utils::{read_ohlcv_from_zip, setup_logger, OhlcvCandle, TimeFrame};
 use optionstratlib::{ExpirationDate, Positive, pos, spos};
 use rust_decimal::Decimal;
 use rust_decimal_macros::dec;
 use tracing::info;
+use optionstratlib::utils::others::calculate_log_returns;
+use optionstratlib::volatility::{annualized_volatility, constant_volatility, historical_volatility};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    setup_logger();
-    let ohlc = read_ohlcv_from_zip("examples/Data/gc-1m.zip", "01/05/2007", "08/05/2008")?;
+fn get_volatilities_from_ohlcv(
+    ohlc: &Vec<OhlcvCandle>,
+) -> Result<(Positive, Vec<Positive>, Vec<Positive>), Box<dyn std::error::Error>> {
     let close_prices = ohlc
         .iter()
         .map(|candle| Positive::new_decimal(candle.close).unwrap())
         .collect::<Vec<_>>();
+
+    let log_returns = calculate_log_returns(&close_prices)?;
+    let binding = log_returns
+        .iter()
+        .map(|&log| annualized_volatility(log, TimeFrame::Minute).unwrap())
+        .map(|log| log.to_dec())
+        .collect::<Vec<Decimal>>();
+    let log_returns_dec = binding.as_slice();
+    let constant_volatility = constant_volatility(log_returns_dec)?;
+    let mut historical_volatility = historical_volatility(log_returns_dec, 60)?;
+    if let Some(&last) = historical_volatility.last() {
+        historical_volatility.extend_from_slice(&[last].repeat(60));
+    }
+    
+    Ok( (
+        constant_volatility,
+        historical_volatility,
+        close_prices
+    ) )
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    setup_logger();
+    // let ohlc = read_ohlcv_from_zip("examples/Data/cl-1m-sample.zip", Some("01/05/2007"), Some("08/05/2008"))?;
+    let ohlc = read_ohlcv_from_zip("examples/Data/cl-1m-sample.zip", None, None)?;
+
+
+    
+    let (constant_volatility, historical_volatility, close_prices) = get_volatilities_from_ohlcv(&ohlc)?;
+    println!("annualized_volatility {:?}", constant_volatility);
+    println!("historical_volatility {:?}", historical_volatility.len());
+    println!("close_prices {:?}", close_prices.len());
+    
+    exit(0);
     let days = pos!(7.0);
     let symbol = "GC".to_string();
     let underlying_price = close_prices[0];
