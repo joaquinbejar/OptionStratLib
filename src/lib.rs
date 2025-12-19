41cbd9950289be63d309cb95e15864c477c7d5c6
g.t. <jakeheke75@gmail.com>
2025-12-19 13:28:43 +0100

    Bump version to 0.14.0 across README, Cargo.toml, and library documentation after Semver check failure.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(unknown_lints)]
 #![allow(clippy::literal_string_with_formatting_args)]
 
-//! # OptionStratLib v0.13.1: Financial Options Library
+//! # OptionStratLib v0.14.0: Financial Options Library
 //!
 //! ## Table of Contents
 //! 1. [Introduction](#introduction)
